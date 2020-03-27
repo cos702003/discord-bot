a03eea12dfa9464c7c89d5df68bde1003b5004b9
Cos sevenzerotwozerozeroseven <jjtt70@hotmail.com>
2020-03-27 11:54:02 -0400

    synching

--- bot.js
+++ bot.js
@@ -1,9 +1,18 @@
+require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.client();
 
 client.on('ready', () =>{
 	console.log('I am ready!');
-};
+);
+
+client.on("message", message =>{
+	if(message.author.bot) return;
+	if(message.content.indexOf(process.env.PREFIX) !==0) return;
+}
+
+const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
+const message = args.shift().toLowerCase();
 
 client.on('message', message => {
 	if(message.content == 'ping') {
